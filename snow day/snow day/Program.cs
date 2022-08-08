07983071fbdbdb0aa6e98a29100d58af0088fe85
EGF9 <finleg1@student.op.ac.nz>
2022-08-09 11:15:35 +1200

    hate git hub re added the same stuff from before

---
+++ snow day/snow day/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace snow_day
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables 
+            string answer = "";
+
+            Console.WriteLine("is it snowing? yes/no");
+            answer = Console.ReadLine();
+
+            if (answer == "yes")
+            {
+                Console.WriteLine("it is a snow day");
+            }
+
+            else
+            {
+                Console.WriteLine("no");
+            }
+            Console.ReadLine();
+        }
+    }
+}
